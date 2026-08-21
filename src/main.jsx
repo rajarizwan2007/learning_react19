87a5555a99f7db71c462fdcaaa8e971b16e9f63f
Raja Rizwan <raja-projectmarker>
2026-08-21 18:39:33 +0500

    unorder list - challenge React practice

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,23 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.querySelector("#root"));
-root.render(<h1>React19 - Learning</h1>);
+const root1 = createRoot(document.querySelector("#root1"));
+root1.render(<h1>Page Title</h1>);
+
+root.render(<ul>
+    <li>
+        1st Item
+    </li>
+    <li>
+        2nd Item
+    </li>
+    <li>
+        3rd Item
+    </li>
+    <li>
+        4th Item
+    </li>
+    <li>
+        5th Item
+    </li>
+</ul>);
