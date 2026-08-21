256646aede76ae3c6daff70dcc6d6ef41bec0f93
Raja Rizwan <raja-projectmarker>
2026-08-21 21:13:59 +0500

    First React Commponent Navigation bar and 2nd component mainContent

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,44 @@
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector("#root"));
-const root1 = createRoot(document.querySelector("#root1"));
-root1.render(<h1>Page Title</h1>);
+function NavBar(){
 
-root.render(<ul>
-    <li>
-        1st Item
-    </li>
-    <li>
-        2nd Item
-    </li>
-    <li>
-        3rd Item
-    </li>
-    <li>
-        4th Item
-    </li>
-    <li>
-        5th Item
-    </li>
-</ul>);
+    return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Features</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Pricing</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>);
+}
+
+function MainContent(){
+    return <h1>Page Header</h1>
+}
+
+const root = createRoot(document.querySelector("#root"));
+
+
+root.render(<div><NavBar />
+<MainContent /></div>);
